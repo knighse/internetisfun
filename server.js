6f1028559bdff00dc2d82bc66c8d42f2d55f6f1e
Sam Knight <knighse@student.okaloosaschools.com>
2019-10-27 17:03:56 -0500

    used a more basic file server

--- server.js
+++ server.js
@@ -1,38 +1,11 @@
 var path = require('path');
 var express = require('express');
 var app = express();
-var fs = require('fs');
 
-var dir = path.join(__dirname, 'public');
+var dir = "/home/robog/internetisfun";
 
-var mime = {
-    html: 'text/html',
-    txt: 'text/plain',
-    css: 'text/css',
-    gif: 'image/gif',
-    jpg: 'image/jpeg',
-    png: 'image/png',
-    svg: 'image/svg+xml',
-    js: 'application/javascript'
-};
+app.use(express.static(dir));
 
-app.get('*', function (req, res) {
-    var file = path.join(dir, req.path.replace(/\/$/, '/index.html'));
-    if (file.indexOf(dir + path.sep) !== 0) {
-        return res.status(403).end('Forbidden');
-    }
-    var type = mime[path.extname(file).slice(1)] || 'text/plain';
-    var s = fs.createReadStream(file);
-    s.on('open', function () {
-        res.set('Content-Type', type);
-        s.pipe(res);
-    });
-    s.on('error', function () {
-        res.set('Content-Type', 'text/plain');
-        res.status(404).end('Not found');
-    });
-});
-
-app.listen(3000, function () {
+app.listen(8080, function () {
     console.log('Listening on http://localhost:3000/');
 });
